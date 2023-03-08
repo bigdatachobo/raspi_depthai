7ac79b8a6a745fef9ca97038f17cc180c0524d12
bigdatachobo <61238033+bigdatachobo@users.noreply.github.com>
2023-03-08 14:39:17 +0900

    Update depthai_setting_3.sh

---
+++ raspi/depthai_setting_3.sh
@@ -0,0 +1,22 @@
+# 위 코드가 잘 실행되면 아래 코드로 진행
+sudo git clone https://github.com/luxonis/depthai.git
+cd depthai
+sudo python3 install_requirements.py
+cd
+echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules
+sudo udevadm control --reload-rules && sudo udevadm trigger
+
+# 설치 확인 예제 코드
+sudo git clone https://github.com/luxonis/depthai-experiments.git
+sudo python3 -m pip install depthai-sdk
+sudo python3 -m pip install --extra-index-url https://www.piwheels.org/simple/ depthai-sdk
+
+# Cortic Technology Python Script Demos
+sudo apt-get install freeglut3-dev
+sudo pip3 install pywavefront
+sudo pip3 install PyOpenGL
+sudo pip3 install pygame_gui -U
+sudo apt install libsdl2-ttf-2.0-0
+sudo pip3 install pywavefront
+sudo git clone https://github.com/cortictechnology/vision_ui.git
+sudo git clone https://github.com/cortictechnology/heartrate_estimation.git
