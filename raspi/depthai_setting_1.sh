38ec1ffbfaff9d3d6d49f502dbf2a32c3585715f
bigdatachobo <61238033+bigdatachobo@users.noreply.github.com>
2023-03-08 14:52:11 +0900

    Update depthai_setting_1.sh

--- raspi/depthai_setting_1.sh
+++ raspi/depthai_setting_1.sh
@@ -14,7 +14,7 @@ conda activate depai
 cd /home/pi/
 sudo curl -fL https://docs.luxonis.com/install_dependencies.sh | bash
 python3 -m pip install depthai
-git clone https://github.com/luxonis/depthai-python.git
+git clone https://github.com/luxonis/depthai-python.git /home/pi/
 cd /home/pi/depthai-python/examples
 sudo python3 install_requirements.py
 cd /home/pi/
