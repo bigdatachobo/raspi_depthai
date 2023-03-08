d1485d163717e9d57f38d4b7636ce8793b0077c2
bigdatachobo <61238033+bigdatachobo@users.noreply.github.com>
2023-03-08 14:40:32 +0900

    Update depthai_setting_1.sh

--- raspi/depthai_setting_1.sh
+++ raspi/depthai_setting_1.sh
@@ -11,13 +11,13 @@ conda create -n depai python=3.8 -y
 conda activate depai
 
 # depth ai
+cd /home/pi/
 sudo curl -fL https://docs.luxonis.com/install_dependencies.sh | bash
 python3 -m pip install depthai
 git clone https://github.com/luxonis/depthai-python.git
-cd depthai-python
-cd examples
+cd /home/pi/depthai-python/examples
 sudo python3 install_requirements.py
-cd
+cd /home/pi/
 sudo pip3 install opencv-python==4.5.1.48
 sudo pip3 install -U numpy
 sudo python3 -m pip install depthai --user
